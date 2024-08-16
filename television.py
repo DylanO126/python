6d51e0fad91b1bdfcfe6485540f6181680135042
DylanO126 <dylanowens126@gmail.com>
2024-08-16 16:59:02 -0500

    First file

--- television.py
+++ television.py
@@ -3,30 +3,52 @@ class Television:
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
-    def __init__(self):
+    def __init__(self) -> None:
+        '''
+        Method to set default values for Television Class
+        '''
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
 
-    def power(self):
+    def power(self) -> None:
+        """
+        Method to switch boolean value of status trait
+        """
         self.__status = bool((self.__status + 1) % 2)
 
-    def mute(self):
+    def mute(self) -> None:
+        """
+        Method to switch boolean value of muted trait
+        """
         if self.__status:
             self.__muted = bool((self.__muted + 1) % 2)
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        """
+        Method to increase the channel trait by 1
+        if at max then restart at zero
+        """
         if self.__status:
             self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        """
+        Method to decrease the channel trait by 1
+        if at min the jump to max
+        """
         if self.__status:
             self.__channel = (self.__channel - 2) % (Television.MAX_CHANNEL + 1)
             self.__channel += 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        """
+        Method to increase the volume trait by 1
+        if at max do nothing
+        if muted = True switch to False
+        """
         if self.__status:
             self.__muted = False
             if self.__volume == Television.MAX_VOLUME:
@@ -34,7 +56,12 @@ class Television:
             else:
                 self.__volume += 1
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        """
+        Method to decrease the volume trait by 1
+        if at min do nothing
+        if muted = True switch to False
+        """
         if self.__status:
             self.__muted = False
             if self.__volume == Television.MIN_VOLUME:
@@ -42,7 +69,12 @@ class Television:
             else:
                 self.__volume -= 1
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Operation Overload for string type for the television class
+        if TV is muted Volume should be Min value
+        :return: formated string according to instructions
+        """
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.MIN_VOLUME}'
         else:
